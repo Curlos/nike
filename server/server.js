aa2f4aff78f251b13024c907d6269e4cec71587b
Carlos Martinez <curlosmart@gmail.com>
2021-09-21 19:25:12 -0400

    setup database

--- server/server.js
+++ server/server.js
@@ -2,11 +2,21 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const axios = require('axios')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 
-const options = {
+const PORT = process.env.port || 8888
+const sneakerRouter = require('./routes/sneakers')
+const database = require('./database/connection')
+
+app.use(cors())
+app.use(express.json())
+app.use(sneakerRouter)
+
+const getAllBrands = {
   method: 'GET',
   url: 'https://v1-sneakers.p.rapidapi.com/v1/sneakers',
-  params: {limit: '20', name: 'Lebron'},
+  params: {limit: '100', brand: 'jordan'},
   headers: {
     'x-rapidapi-host': 'v1-sneakers.p.rapidapi.com',
     'x-rapidapi-key': '4a1ae12979msh723335e2f4d235ep12496ejsn5cc07d412b1c'
@@ -15,10 +25,17 @@ const options = {
 
 axios.request(options)
   .then((response) => {
-    console.log(response.data);
+    console.log(JSON.stringify(response.data, null, 2))
   }).catch(function (error) {
     console.error(error);
   });
 
 
-app.listen(PORT || 8888, () => console.log(`Sneaker server listening on port ${PORT}`))
+app.listen(PORT, () => {
+  database.connectToServer((err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+  console.log(`Sneaker server listening on port ${PORT}`)
+})
