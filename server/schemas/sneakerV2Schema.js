3e889240d943f339f3f4c789717971c814adb7fb
Carlos Martinez <curlosmart@gmail.com>
2021-09-22 11:16:55 -0400

    schema for sneakers v2

---
+++ server/schemas/sneakerV2Schema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const sneakerV2Schema = new mongoose.Schema(
+  {
+    sneakerID: {type: String, required: true},
+    sku: {type: String, required: true},
+    brand: {type: String, required: true},
+    name: {type: String, required: true},
+    colorway: {type: String},
+    gender: {type: String, required: true},
+    silhouette: {type: String, required: true},
+    releaseYear: {type: Number, required: true},
+    releaseDate: {type: String},
+    retailPrice: {type: Number, required: true},
+    estimatedMarketValue: {type: Number, required: true},
+    story: {type: String},
+    image: {
+      "360": {type: Array},
+      original: {type: String, required: true},
+      small: {type: String, required: true},
+      thumbnail: {type: String, required: true},
+    },
+    links: {
+      stockX: {type: String},
+      goat: {type: String},
+      flightClub: {type: String},
+      stadiumGoods: {type: String},
+    },
+  }
+)
+
+module.exports = sneakerV2Schema;
