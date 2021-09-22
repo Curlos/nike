1d10d30c092bf8fc5c6043c540d6569d22cefb72
Carlos Martinez <curlosmart@gmail.com>
2021-09-21 20:02:25 -0400

    Can get all sneaker data from API

---
+++ server/routes/sneakerDBSetup.js
@@ -0,0 +1,71 @@
+const axios = require('axios')
+
+const sneakers = {}
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+const getAllBrands = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://v1-sneakers.p.rapidapi.com/v1/brands',
+    headers: {
+      'x-rapidapi-host': 'v1-sneakers.p.rapidapi.com',
+      'x-rapidapi-key': '4a1ae12979msh723335e2f4d235ep12496ejsn5cc07d412b1c'
+    }
+  }
+  
+  try {
+    const response = await axios.request(options)
+    const allBrands = await response.data.results
+    return allBrands
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const getSneakersFromBrand = async (brand) => {
+  const options = {
+    method: 'GET',
+    url: 'https://v1-sneakers.p.rapidapi.com/v1/sneakers',
+    params: {limit: '100', brand: brand},
+    headers: {
+      'x-rapidapi-host': 'v1-sneakers.p.rapidapi.com',
+      'x-rapidapi-key': '4a1ae12979msh723335e2f4d235ep12496ejsn5cc07d412b1c'
+    }
+  }
+  
+  try {
+    const response = await axios.request(options)
+    console.log(JSON.stringify(response.data, null, 4))
+    return response.data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const getSneakersFromAllBrands = async () => {
+  const allBrands = await getAllBrands()
+  await timer(2000)
+
+  for (let brand of allBrands) {
+    if (brand === 'AIR JORDAN') {
+      const brandSneakers = await getSneakersFromBrand('Jordan')
+      sneakers[brand] = brandSneakers
+      console.log(brand)
+      console.log('---')
+    } else {
+      const brandSneakers = await getSneakersFromBrand(brand)
+      sneakers[brand] = brandSneakers
+      console.log(brand)
+      console.log('---')
+    }
+
+    await timer(2000)
+  }
+
+  console.log(sneakers)
+}
+
+// getSneakersFromAllBrands()
+
+getSneakersFromBrand('Jordan')
--- server/server.js
+++ server/server.js
@@ -13,23 +13,6 @@ app.use(cors())
 app.use(express.json())
 app.use(sneakerRouter)
 
-const getAllBrands = {
-  method: 'GET',
-  url: 'https://v1-sneakers.p.rapidapi.com/v1/sneakers',
-  params: {limit: '100', brand: 'jordan'},
-  headers: {
-    'x-rapidapi-host': 'v1-sneakers.p.rapidapi.com',
-    'x-rapidapi-key': '4a1ae12979msh723335e2f4d235ep12496ejsn5cc07d412b1c'
-  }
-};
-
-axios.request(options)
-  .then((response) => {
-    console.log(JSON.stringify(response.data, null, 2))
-  }).catch(function (error) {
-    console.error(error);
-  });
-
 
 app.listen(PORT, () => {
   database.connectToServer((err) => {
