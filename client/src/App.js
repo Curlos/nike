84576822fb1c420c6f845b6b35457b598c392326
Carlos Martinez <curlosmart@gmail.com>
2021-09-22 20:27:08 -0400

    moved routes to parent component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,61 @@
-import React, { useState } from 'react-router-dom'
+import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Navbar from './components/Navbar'
+import Shoes from './components/Shoes'
+import FullShoe from './components/FullShoe'
 import './style.scss';
 
 function App() {
+
+  const [selectedShoe, setSelectedShoe] = useState({})
+
+  const handleSelectShoe = (shoeObj) => {
+    setSelectedShoe(shoeObj)
+  }
+
   return (
-    <div>
-      <Navbar />
-    </div>
+
+    <Router>
+      <div>
+        <Navbar />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route path="/new-releases" exact>
+            New Releases
+          </Route>
+          <Route path="/men" exact>
+            Men
+          </Route>
+          <Route path="/women" exact>
+            Women
+          </Route>
+          <Route path="/kids" exact>
+            Kids
+          </Route>
+          <Route path="/brands" exact>
+            Explore Brands
+          </Route>
+          <Route path="/brands/:brand" exact>
+            <Shoes handleSelectShoe={handleSelectShoe}/>
+          </Route>
+
+          <Route path="/shoe/:id" exact>
+            <FullShoe shoeObj={selectedShoe}/>
+          </Route>
+
+          <Route path="/">
+            Home
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
