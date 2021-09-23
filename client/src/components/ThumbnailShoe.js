55e866ec4a06ac0e708a0ba89d3acc3284980c66
Carlos Martinez <curlosmart@gmail.com>
2021-09-22 20:27:26 -0400

    Thumbnail Shoe Component

---
+++ client/src/components/ThumbnailShoe.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+
+const ThumbnailShoe = ({ shoeObj, handleSelectShoe }) => {
+
+  const handleClick = () => {
+    handleSelectShoe(shoeObj)
+  }
+  return (
+    <div class="singleShoeContainer">
+      <Link to={`/shoe/${shoeObj.sneakerID}`} onClick={handleClick}>
+        <img src={shoeObj.image.thumbnail} alt={shoeObj.name} />
+        <div>{shoeObj.name}</div> 
+      </Link>
+    </div>
+  )
+}
+
+export default ThumbnailShoe;
