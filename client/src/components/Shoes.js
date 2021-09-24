c0e2ad93691c59fcbb3a2e266c339c6c98d08016
Carlos Martinez <curlosmart@gmail.com>
2021-09-24 01:13:07 -0400

    sorting algorithm and filtering added

--- client/src/components/Shoes.js
+++ client/src/components/Shoes.js
@@ -1,55 +1,132 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from "react-router-dom";
 import ThumbnailShoe from './ThumbnailShoe'
+import Sidebar from './Sidebar'
 import axios from 'axios'
 
-const Shoes = ({ finalizedSearchQuery, resetFinalizedSearchQuery, handleSelectShoe }) => {
+const Shoes = ({ brands, finalizedSearchQuery, resetFinalizedSearchQuery, handleSelectShoe, handleSelectBrand, lastFilterChange }) => {
 
   const [shoes, setShoes] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-  const { brand } = useParams()
+
+  console.log(shoes)
+  console.log(brands)
+  
+
+  useEffect(() => {
+    // Every time the brand name changes, reset the search query
+    resetFinalizedSearchQuery()
+  }, [brands])
 
   useEffect(() => {
 
+    // Every time a new search is submitted, filter shoes based on the query
+    console.log(lastFilterChange)
+    console.log(finalizedSearchQuery)
+
     const fetchFromServer = async () => {
       if (finalizedSearchQuery === '') {
-        const response = await axios.get(`http://localhost:3001/sneakers/brands/${brand.toUpperCase()}`)
-        const data = response.data
+        if (lastFilterChange.checked === true) {
+          const response = await axios.get(`http://localhost:3001/sneakers/brands/${lastFilterChange.name.toUpperCase()}`)
+          const data = response.data
+          console.log(data)
+          const newShoes = [...shoes]
+          newShoes.push(...data)
+          // sortShoesByRelease(newShoes)
+          sortShoesFromLowestToHighestPrice(newShoes)
 
-        console.log(data)
-
-        setShoes(data)
-        setIsLoading(false) 
+          setShoes(newShoes)
+          setIsLoading(false) 
+        } else {
+          console.log('unchecked')
+          const newShoes = filterOutShoes({brand: lastFilterChange.name})
+          // sortShoesByRelease(newShoes)
+          sortShoesFromLowestToHighestPrice(newShoes)
+          setShoes(newShoes)
+        }
       } else {
-        const body = {
+        const filters = {
           "name": finalizedSearchQuery
         }
-        const response = await axios.post(`http://localhost:3001/sneakers/brands/${brand.toUpperCase()}/filter`, body)
-        const data = response.data
 
-        console.log(data)
+        const newShoes = filterShoes(filters)
+        console.log(newShoes)
 
-        setShoes(data)
+        setShoes(newShoes)
         setIsLoading(false)
-        // resetFinalizedSearchQuery() 
       }
     }
     fetchFromServer()
-  }, [finalizedSearchQuery, brand])
+  }, [finalizedSearchQuery, brands])
+
+  const filterOutShoes = (filters) => {
+    const newShoes = shoes.filter((shoe) => {
+      for (let prop of Object.keys(filters)) {
+        console.log(`${shoe[prop]} === ${filters[prop]} ${shoe[prop] === filters[prop]}`)
+        if (shoe[prop].toLowerCase() === filters[prop].toLowerCase()) {
+          return false
+        }
+      }
+      return true
+    })
+
+    return newShoes
+  }
+
+  const filterShoes = (filters) => {
+    const newShoes = shoes.filter((shoe) => {
+      for (let prop of Object.keys(filters)) {
+        console.log(`${shoe[prop]} === ${filters[prop]} ${shoe[prop] === filters[prop]}`)
+        if (prop === 'name' && !shoe[prop].includes(filters[prop])) {
+          return false
+        } else if (shoe[prop].toLowerCase() !== filters[prop].toLowerCase()) {
+          return false
+        }
+      }
+      return true
+    })
+
+    console.log(newShoes)
+
+    return newShoes
+  }
+
+  const sortShoesByRelease = (newShoes) => {
+    return newShoes.sort((a, b) => {
+      if (a.releaseYear < b.releaseYear) {
+        return 1
+      } else if (new Date(a.releaseDate) < new Date(b.releaseDate)) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+  }
+
+  const sortShoesFromHighestToLowestPrice = (newShoes) => {
+    return newShoes.sort((a, b) => (a.retailPrice < b.retailPrice) ? 1 : -1)
+  }
+
+  const sortShoesFromLowestToHighestPrice = (newShoes) => {
+    return newShoes.sort((a, b) => (a.retailPrice > b.retailPrice) ? 1 : -1)
+  }
 
 
   return (
 
-    <div class="shoesContainer">
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        shoes.map((shoeObj) => {
-         return (
-          <ThumbnailShoe key={shoeObj.sneakerID} shoeObj={shoeObj} handleSelectShoe={handleSelectShoe}/>
-         )
-        })
-      )}
+    <div className="shoesPage">
+      <Sidebar handleSelectBrand={handleSelectBrand}/>
+
+      <div className="shoesContainer">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          shoes.map((shoeObj) => {
+          return (
+            <ThumbnailShoe key={shoeObj.sneakerID} shoeObj={shoeObj} handleSelectShoe={handleSelectShoe}/>
+          )
+          })
+        )}
+      </div>
     </div>
   )
 }
