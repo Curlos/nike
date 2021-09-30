1110e8e918b5b11c75e2c4c61b8d88d24c53754c
Carlos Martinez <curlosmart@gmail.com>
2021-09-29 22:54:35 -0400

    when price or color clicked, state change

--- client/src/App.js
+++ client/src/App.js
@@ -15,9 +15,16 @@ const App = () => {
   const [selectedShoe, setSelectedShoe] = useState({})
   const [finalizedSearchQuery, setFinalizedSearchQuery] = useState('')
   const [brands, setBrands] = useState({})
-  const [lastFilterChange, setLastFilterChange] = useState({})
-  const [filters, setFilters] = useState({})
   const [sortType, setSortType] = useState('Newest')
+  const [shoeCount, setShoeCount] = useState(0)
+  const [lastFilterChange, setLastFilterChange] = useState({})
+  const [filters, setFilters] = useState({
+    'priceRanges': {'$0 - $50': {min: 0, max: 50, checked: false}, '$50 - $100': {min: 50, max: 100, checked: false}, '$100 - $150': {min: 100, max: 150, checked: false}, 'Over $150': {min: 150, checked: false}},
+    
+    'colors': {'black': false, 'blue': false, 'brown': false, 'green': false, 'grey': false, 'multi-color': false, 'orange': false, 'pink': false, 'purple': false, 'red': false, 'white': false, 'yellow': false},
+
+    'name': ''
+  })
 
   console.log(filters)
 
@@ -32,10 +39,24 @@ const App = () => {
     console.log(newBrand)
   }
 
+  const clearLastFilterChange = () => {
+    setLastFilterChange('')
+  } 
+
   const handleSearch = (searchQuery) => {
     console.log(searchQuery)
     setFinalizedSearchQuery(searchQuery)
     setFilters({...filters, name: finalizedSearchQuery})
+  }
+
+  const handleFilterChange = (filterType, newFilterValue) => {
+    setFilters({...filters, [filterType]: newFilterValue})
+
+    console.log(filters)
+  }
+
+  const handleShoeCount = (newShoeCount) => {
+    setShoeCount(newShoeCount)
   }
 
   const resetFinalizedSearchQuery = () => {
@@ -74,7 +95,7 @@ const App = () => {
           </Route>
           
           <Route path="/shoes" exact>
-            <Shoes brands={brands} handleSearch={handleSearch} finalizedSearchQuery={finalizedSearchQuery} resetFinalizedSearchQuery={resetFinalizedSearchQuery} handleSelectShoe={handleSelectShoe} handleSelectBrand={handleSelectBrand} lastFilterChange={lastFilterChange} handleSort={handleSort} sortType={sortType}/>
+            <Shoes brands={brands} handleSearch={handleSearch} finalizedSearchQuery={finalizedSearchQuery} resetFinalizedSearchQuery={resetFinalizedSearchQuery} handleSelectShoe={handleSelectShoe} handleSelectBrand={handleSelectBrand} filters={filters} lastFilterChange={lastFilterChange} clearLastFilterChange={clearLastFilterChange} handleSort={handleSort} sortType={sortType} handleFilterChange={handleFilterChange} shoeCount={shoeCount} handleShoeCount={handleShoeCount}/>
           </Route>
 
           <Route path="/shoe/:brand/:sneakerID" exact>
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -31,6 +31,15 @@ const ALL_BRANDS = [
   "YEEZY",
 ]
 
+const PRICE_RANGES = [
+  { stringRange: "$0 - $50", min: 0, max: 50 }, 
+  { stringRange: "$50 - $100", min: 50, max: 100 },
+  { stringRange: "$100 - $150", min: 100, max: 150 },
+  { stringRange: "Over $150", min: 150 },
+]
+
+const COLORS = ['black', 'blue', 'brown', 'green', 'grey', 'multi-color', 'orange', 'pink', 'purple', 'red', 'white', 'yellow']
+
 const titleCase = (str) => {
 
   if(str.includes('-')) {
@@ -44,15 +53,29 @@ const titleCase = (str) => {
   .join(' ')
 }
 
-const Sidebar = ({ handleSelectBrand, brands }) => {
+const Sidebar = ({ handleSelectBrand, brands, handleFilterChange, filters }) => {
 
 
-  const handleClick = (e) => {
+  const handleBrandClick = (e) => {
     const newBrandName = e.target.value
     const checked = e.target.checked
     const newBrand = {name: newBrandName, checked: checked}
     console.log(brands)
     handleSelectBrand(newBrand)
+  }
+
+  const handlePriceClick = (e) => {
+    const stringRange = e.target.name
+
+    const newPriceRange = {...filters['priceRanges'][stringRange], checked: !filters['priceRanges'][stringRange].checked}
+
+    handleFilterChange('priceRanges', {...filters['priceRanges'], [stringRange]: newPriceRange})
+  }
+
+  const handleColorClick = (e) => {
+    const color = e.target.name.toLowerCase()
+
+    handleFilterChange('colors', {...filters['colors'], [color]: !filters['colors'][color]})
   }
 
 
@@ -62,12 +85,32 @@ const Sidebar = ({ handleSelectBrand, brands }) => {
       {ALL_BRANDS.map((brand) => {
         return (
           <a href="/">
-            <input type="checkbox" name={titleCase(brand)} onClick={handleClick} value={titleCase(brand)} checked={brands[titleCase(brand)] && brands[titleCase(brand)].checked}/>
+            <input type="checkbox" name={titleCase(brand)} onClick={handleBrandClick} value={titleCase(brand)} checked={brands[titleCase(brand)] && brands[titleCase(brand)].checked}/>
             <label for={titleCase(brand)}>{titleCase(brand)}</label>
           </a>
         )
       })}
 
+      <div class="line-separator"></div>
+
+      {PRICE_RANGES.map((priceRange) => {
+
+       return (
+        <a href="/">
+          <input type="checkbox" name={priceRange.stringRange} onClick={handlePriceClick} minPrice={priceRange.min} maxPrice={priceRange.max} checked={filters['priceRanges'][priceRange.stringRange].checked} />
+          <label for={priceRange.stringRange}>{priceRange.stringRange}</label>
+        </a>
+       )
+      })}
+
+      {COLORS.map((color) => {
+        return (
+          <a href="/">
+            <input type="checkbox" name={titleCase(color)} onClick={handleColorClick} value={color} checked={filters['colors'][color]} />
+            <label for={titleCase(color)}>{titleCase(color)}</label>
+          </a>
+        )
+      })}
 
       
     </div>
