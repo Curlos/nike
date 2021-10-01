405373cf01e70c5538f9603a441de367117874ff
Carlos Martinez <curlosmart@gmail.com>
2021-09-30 23:25:29 -0400

    login form

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Link } from "react-router-dom"
+
+const SERVER_URL = 'http://localhost:3001'
+
+
+const LoginForm = () => {
+  const [emailAddress, setEmailAddress] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(emailAddress)
+    console.log(password)
+
+    const user = {
+      email: emailAddress,
+      password: password
+    }
+
+    const data = await axios.post(SERVER_URL + '/login', user)
+    console.log(data)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmailAddress(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  return (
+    <form class="loginForm">
+      <input name="email" type="email" placeholder="Email address" value={emailAddress} onChange={handleEmailChange}/>
+      <input name="password" type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/>
+      <button onClick={handleSubmit}>SIGN IN</button>
+    </form>
+  )
+  
+}
+
+export default LoginForm;
