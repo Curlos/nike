51bc36148fedf8c7e3e95aabdc97828e9c80b5a3
Carlos Martinez <curlosmart@gmail.com>
2021-10-11 20:43:00 -0400

    User and Sneaker models updated

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,14 @@ const User = mongoose.model(
   "User",
   new mongoose.Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    reviews: [{ type: String, required: true }],
+    shoeFavorites: [{ type: String, required: true }],
+    reviewUpvotes: [{ type: String, required: true }],
+    reviewDownvotes: [{ type: String, required: true }],
+    lowerCaseEmail: {type: String, lowercase: true, trim: true, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
   })
 );
 
